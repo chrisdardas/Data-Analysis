417ddfbfd2e3185ca31eff7be0a1f13728441ee1
chrisdardas <85883633+chrisdardas@users.noreply.github.com>
2024-11-21 21:51:47 +0200

    Changes & New Exercises

--- Chapter 5/exercise5_2.m
+++ Chapter 5/exercise5_2.m
@@ -9,23 +9,23 @@ mX = 0;
 mY = 0;
 sigmaX = 1;
 sigmaY = 1;
+M = 100;
 
 % For (X, Y) and ρ = 0
 rho = 0;
 squared = 0;
-function5_2(n, L, rho, mX, mY, sigmaX, sigmaY, alpha, squared);
+function5_2(n, L, rho, mX, mY, sigmaX, sigmaY, alpha, squared, M);
 
 % For (X, Y) and ρ = 0.5
 rho = 0.5;
-function5_2(n, L, rho, mX, mY, sigmaX, sigmaY, alpha, squared);
+function5_2(n, L, rho, mX, mY, sigmaX, sigmaY, alpha, squared, M);
 
 % For (X^2, Y^2) and ρ = 0
 rho = 0;
 squared = 1;
-function5_2(n, L, rho, mX, mY, sigmaX, sigmaY, alpha, squared);
+function5_2(n, L, rho, mX, mY, sigmaX, sigmaY, alpha, squared, M);
 
 % For (X^2, Y^2) and ρ = 0.5
 rho = 0.5;
-function5_2(n, L, rho, mX, mY, sigmaX, sigmaY, alpha, squared);
-
+function5_2(n, L, rho, mX, mY, sigmaX, sigmaY, alpha, squared, M);
 
--- Chapter 5/function5_2.m
+++ Chapter 5/function5_2.m
@@ -1,4 +1,4 @@
-function [] = function5_2(n, L, rho, mX, mY, sigmaX, sigmaY, alpha, squared)
+function [] = function5_2(n, L, rho, mX, mY, sigmaX, sigmaY, alpha, squared, M)
 
 
 if squared == 1
@@ -13,40 +13,42 @@ upper_limit = L*(1 - alpha/2);
 R = mvnrnd(mu, Sigma, n);
 X = R(:, 1);
 Y = R(:,2);
+rejected = 0;
 
-if squared == 1
-    X = X.^2;
-    Y = Y.^2;
-end
-
-Cov0 = corrcoef(X, Y);
-r0 = Cov0(1, 2);
-
-tc0 = r0 * sqrt((n-2) / (1 - r0^2));
-
-t_stats = zeros(L, 1);
-for i=1:L
-    idX = randperm(n);
-    X_new = R(idX, 1);
+for j=1:M
     if squared == 1
-        X_new = X_new.^2;
+        X = X.^2;
+        Y = Y.^2;
     end
-
-    Cov = corrcoef(X_new, Y);
-
-    r = Cov(1, 2);
-    t_stats(i) = r * sqrt((n-2) / (1 - r^2));
+    
+    Cov0 = corrcoef(X, Y);
+    r0 = Cov0(1, 2);
+    
+    tc0 = r0 * sqrt((n-2) / (1 - r0^2));
+    
+    t_stats = zeros(L, 1);
+    for i=1:L
+        idX = randperm(n);
+        X_new = R(idX, 1);
+        if squared == 1
+            X_new = X_new.^2;
+        end
+    
+        Cov = corrcoef(X_new, Y);
+    
+        r = Cov(1, 2);
+        t_stats(i) = r * sqrt((n-2) / (1 - r^2));
+    end
+    
+    t_stats = sort(t_stats, 1);
+    
+    t_lower = t_stats(lower_limit);
+    t_upper = t_stats(upper_limit);
+    
+    if (tc0 < t_lower || tc0 > t_upper)
+        rejected = rejected + 1;
+    end
 end
 
-t_stats = sort(t_stats, 1);
-
-t_lower = t_stats(lower_limit);
-t_upper = t_stats(upper_limit);
-
-if (tc0 < t_lower && tc0 > t_upper)
-    fprintf("The Null Hypothesis H0 is rejected\n");
-else
-    fprintf("We can not reject the Null Hypothesis H0\n");
-end
-
+fprintf("The Null Hypothesis is rejected with %.2f%% percentage\n", rejected);
 end
